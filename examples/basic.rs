cb4c63ffbed516c8f32afadba07e9c7e74c19547
GeekMasher <geekmasher@gmail.com>
2024-08-07 12:19:40 +0100

    feat(examples): Update basic

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,19 +3,29 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     // Load environment variables from .env file
-    dotenvy::dotenv()?;
+    // dotenvy::dotenv()?;
 
     // Load the configuration (from environment variables)
-    let config = octoapp::OctoAppConfig::init().build()?;
+    let config = octoapp::OctoAppConfig::init().app_id(12345).build()?;
     println!("{}", config);
 
-    let octocrab = config.octocrab()?;
-    octocrab.installation(octocrab::models::InstallationId(config.app_id() as u64));
+    let octocrab = config.octocrab();
+    if let Ok(client) = octocrab {
+        client.installation(octocrab::models::InstallationId(config.app_id() as u64));
+        println!("{:?}", client);
 
-    println!("{:?}", octocrab);
+        let org = client.orgs("42ByteLabs").get().await?;
+        println!("{:?}", org);
 
-    let org = octocrab.orgs("42ByteLabs").get().await?;
-    println!("{:?}", org);
+        let repos = client
+            .issues("42ByteLabs", "octoapp")
+            .list()
+            .creator("GeekMasher")
+            .send()
+            .await?;
+
+        println!("Reposiotories: {}", repos.total_count.unwrap_or_default());
+    }
 
     Ok(())
 }
